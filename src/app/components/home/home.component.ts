6deac1e50616ef283406485b6247fd9618e86442
CristianoDinh <dinhgiabao1142004@gmail.com>
2025-03-26 09:45:26 +0700

    add sponsor section

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,6 +3,7 @@ import {BannerComponent} from '../banner/banner.component';
 import {MatchesComponent} from '../matches/matches.component';
 import {NewsComponent} from '../news/news.component';
 import {LineUpComponent} from '../line-up/line-up.component';
+import {SponsorComponent} from '../sponsor/sponsor.component';
 
 
 @Component({
@@ -12,6 +13,7 @@ import {LineUpComponent} from '../line-up/line-up.component';
     MatchesComponent,
     NewsComponent,
     LineUpComponent,
+    SponsorComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
