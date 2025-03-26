a542d2b4a5aac392903208fe9ea6172d8d9eb0d2
CristianoDinh <dinhgiabao1142004@gmail.com>
2025-03-26 15:27:24 +0700

    add footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  imports: [
+    NgIf
+  ],
+  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
 
+  // Biến để kiểm soát trạng thái hiển thị của nút
+  isButtonVisible: boolean = false;
+
+  // Lắng nghe sự kiện cuộn
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+    // Hiển thị nút khi cuộn xuống hơn 200px
+    this.isButtonVisible = scrollPosition > 200;
+  }
+
+  // Cuộn lên đầu trang khi nhấn nút
+  scrollToTop(): void {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // Hiệu ứng cuộn mượt
+    });
+  }
 }
