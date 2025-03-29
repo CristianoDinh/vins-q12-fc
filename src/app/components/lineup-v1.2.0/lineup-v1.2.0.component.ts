36a018703c0a7086155e4f78559d623d76a2b206
CristianoDinh <dinhgiabao1142004@gmail.com>
2025-03-29 10:02:28 +0700

    update line-up and squad

--- src/app/components/lineup-v1.2.0/lineup-v1.2.0.component.ts
+++ src/app/components/lineup-v1.2.0/lineup-v1.2.0.component.ts
@@ -1,24 +1,27 @@
 import {Component, inject} from '@angular/core';
 import {Player} from '../../interface/player';
 import {PlayerService} from '../../services/player.service';
-import {NgForOf} from '@angular/common';
+import {NgForOf, NgStyle} from '@angular/common';
 import {NextDirective} from '../../directives/next.directive';
 
 @Component({
   selector: 'app-lineup',
   imports: [
     NextDirective,
-    NgForOf
+    NgForOf,
+    NgStyle
   ],
   templateUrl: './lineup-v1.2.0.component.html',
   styleUrl: './lineup-v1.2.0.component.css'
 })
 export class LineupComponent {
   playerList: Player[] = [];
-  playerService: PlayerService = inject(PlayerService);
+  playerList_NoRumour: Player[] = [];
 
+  playerService: PlayerService = inject(PlayerService);
   constructor() {
     this.playerList = this.playerService.getAllPlayers();
+    this.playerList_NoRumour = this.playerService.getAllPlayers_NoRumour();
   }
 
 }
--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -18,13 +18,24 @@ export class PlayerService {
     { id: 16, name: 'Ẽndick', position: 'Forward', imgUrl: 'squad-1.png' },
     { id: 20, name: 'Kobbie Do', position: 'Forward', imgUrl: 'squad-1.png' },
 
-    // { id: 30, name: 'Quân Palmer', position: 'Forward', imgUrl: 'squad-1.png' },
-    // { id: 31, name: 'Secrete', position: 'Goalkeeper', imgUrl: 'squad-1.png' },
-    // { id: 32, name: 'Secrete', position: 'Goalkeeper', imgUrl: 'squad-1.png' },
-    // { id: 33, name: 'Secrete', position: 'Goalkeeper', imgUrl: 'squad-1.png' }
+    { id: 30, name: 'Quân Palmer', position: 'Midfielder', imgUrl: 'squad-1.png' },
+    // { id: 31, name: 'Secrete', position: 'GK', imgUrl: 'player-secrete-gk.png' },
+    // { id: 32, name: 'Secrete', position: 'Mid', imgUrl: 'player-secrete-mid.png' },
+    // { id: 33, name: 'Secrete', position: 'For', imgUrl: 'player-secrete-forward.png' },
+
+    { id: 0, name: 'RUMOUR', position: 'Goalkeeper1', imgUrl: 'secrete-player.png' },
+    { id: 0, name: 'RUMOUR', position: 'Defender1', imgUrl: 'secrete-player.png' },
+    { id: 0, name: 'RUMOUR', position: 'Forward1', imgUrl: 'secrete-player.png' },
   ];
 
   getAllPlayers() {
     return this.playerList;
   }
+
+  getAllPlayers_NoRumour() {
+    return this.playerList.filter(player =>
+      player.position === 'Goalkeeper' || player.position === 'Defender' || player.position === 'Midfielder' || player.position === 'Forward'
+    );
+  }
+
 }
