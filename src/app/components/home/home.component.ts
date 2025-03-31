bbb24bcc55e49443037bc1e5a59f78f6b93ae6f7
CristianoDinh <dinhgiabao1142004@gmail.com>
2025-04-01 01:49:43 +0700

    update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,7 +3,6 @@ import {BannerComponent} from '../banner/banner.component';
 import {MatchesComponent} from '../matches-intro/matches.component';
 import {NewsComponent} from '../news-intro/news.component';
 import {LineupComponent} from '../lineup-v1.2.0/lineup-v1.2.0.component';
-import {SponsorComponent} from '../sponsor/sponsor.component';
 
 @Component({
   selector: 'app-home',
@@ -12,7 +11,6 @@ import {SponsorComponent} from '../sponsor/sponsor.component';
     MatchesComponent,
     NewsComponent,
     LineupComponent,
-    SponsorComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
