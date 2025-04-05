1ddf1572f8fc2d5472dbedc592f0a14d057817c5
CristianoDinh <dinhgiabao1142004@gmail.com>
2025-04-05 18:27:40 +0700

    add celebration

--- src/app/interface/player.ts
+++ src/app/interface/player.ts
@@ -3,4 +3,5 @@ export interface Player {
   name: string;
   position: string;
   imgUrl: string;
+  celebrationUrl: string;
 }
--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -8,24 +8,24 @@ export class PlayerService {
   constructor() { }
 
   protected playerList: Player[] = [
-    { id: 1, name: 'Lëv YaBao', position: 'Goalkeeper', imgUrl: 'squad-1-yabao.png' },
-    { id: 7, name: 'Rô Nam Đô', position: 'Midfielder', imgUrl: 'squad-unknown.png' },
-    { id: 8, name: 'Cùcù re Q`uan', position: 'Defender', imgUrl: 'squad-8-cucu.png' },
-    { id: 9, name: 'Øduongaard', position: 'Midfielder', imgUrl: 'squad-1.png' },
+    { id: 1, name: 'Lëv YaBao', position: 'Goalkeeper', imgUrl: 'squad-1-yabao.png', celebrationUrl: 'celeb-1-yabao-3.gif' },
+    { id: 7, name: 'Rô Nam Đô', position: 'Midfielder', imgUrl: 'squad-unknown.png', celebrationUrl: '' },
+    { id: 8, name: 'Cùcù re Q`uan', position: 'Defender', imgUrl: 'squad-8-cucu.png', celebrationUrl: '' },
+    { id: 9, name: 'Øduongaard', position: 'Midfielder', imgUrl: 'squad-1.png', celebrationUrl: '' },
 
-    { id: 10, name: "Mes'Son", position: 'Defender', imgUrl: 'squad-1.png' },
-    { id: 13, name: 'HarriQotter', position: 'Midfielder', imgUrl: 'squad-1.png' },
-    { id: 16, name: 'Ẽndick', position: 'Forward', imgUrl: 'squad-1.png' },
-    { id: 20, name: 'Kobbie Do', position: 'Forward', imgUrl: 'squad-1.png' },
+    { id: 10, name: "Mes'Son", position: 'Defender', imgUrl: 'squad-1.png', celebrationUrl: '' },
+    { id: 13, name: 'HarriQotter', position: 'Midfielder', imgUrl: 'squad-1.png', celebrationUrl: '' },
+    { id: 16, name: 'Ẽndick', position: 'Forward', imgUrl: 'squad-1.png', celebrationUrl: '' },
+    { id: 20, name: 'Kobbie Do', position: 'Forward', imgUrl: 'squad-1.png', celebrationUrl: '' },
 
-    { id: 30, name: 'Quân Palmer', position: 'Midfielder', imgUrl: 'squad-1.png' },
+    { id: 30, name: 'Quân Palmer', position: 'Midfielder', imgUrl: 'squad-1.png', celebrationUrl: '' },
     // { id: 31, name: 'Secrete', position: 'GK', imgUrl: 'player-secrete-gk.png' },
     // { id: 32, name: 'Secrete', position: 'Mid', imgUrl: 'player-secrete-mid.png' },
     // { id: 33, name: 'Secrete', position: 'For', imgUrl: 'player-secrete-forward.png' },
 
-    { id: 0, name: 'RUMOUR', position: 'Goalkeeper1', imgUrl: 'secrete-player.png' },
-    { id: 0, name: 'RUMOUR', position: 'Defender1', imgUrl: 'secrete-player.png' },
-    { id: 0, name: 'RUMOUR', position: 'Forward1', imgUrl: 'secrete-player.png' },
+    { id: 0, name: 'RUMOUR', position: 'Goalkeeper1', imgUrl: 'secrete-player.png', celebrationUrl: '' },
+    { id: 0, name: 'RUMOUR', position: 'Defender1', imgUrl: 'secrete-player.png', celebrationUrl: '' },
+    { id: 0, name: 'RUMOUR', position: 'Forward1', imgUrl: 'secrete-player.png', celebrationUrl: '' },
   ];
 
   getAllPlayers() {
