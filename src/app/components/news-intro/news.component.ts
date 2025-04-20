31da5a77dfb78046fd6cfb1dc5a1679d6e74dedf
CristianoDinh <dinhgiabao1142004@gmail.com>
2025-04-20 17:27:48 +0700

    update news intro

--- src/app/components/news-intro/news.component.ts
+++ src/app/components/news-intro/news.component.ts
@@ -1,14 +1,17 @@
 import {Component, OnInit} from '@angular/core';
-import {NgIf} from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
 import {News} from '../../interface/news';
 import {NewsService} from '../../services/news.service';
 import {DateFormatPipe} from '../../pipes/date-format.pipe';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-news',
   imports: [
     NgIf,
-    DateFormatPipe
+    DateFormatPipe,
+    NgForOf,
+    RouterLink
   ],
   templateUrl: './news.component.html',
   styleUrl: './news.component.css'
